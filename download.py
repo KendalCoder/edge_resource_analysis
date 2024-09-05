3ed629f43709d1b3b049e3eaa0ffa1ed85177625
gemblerz <ironyonghokim@gmail.com>
2024-09-05 12:50:51 -0500

    Added perf subcommand for downloading performance data

--- download.py
+++ download.py
@@ -13,11 +13,14 @@ def cli():
 @click.option("-v", "--vsn", required=True, type=str, help="VSN of the node.")
 @click.option("-s", "--start", required=True, help="Start time of the query in UTC, e.g. 1h, 10d, 2024-01-01T00:00:00Z")
 @click.option("-e", "--end", default="", help="End time of the query in UTC, e.g. 1h, 10d, 2024-01-02T00:00:00Z")
+@click.option("-o", "--output", type=Path, default=Path("jobs.csv"), help="Path to save the downloaded data. Default is jobs.csv")
 @click.option("-b", "--bulk", is_flag=True, default=False, help="Enable downloading data in splits. This helps lowering server-side loads when downloading with a large time window such as months. The default time window in the split is day.")
-def job(vsn, start, end, bulk):
+def job(vsn, start, end, output, bulk):
     start_t, err = parse_time(start)
     end_t, err = parse_time(end)
+    logging.info(f'Query ranges from {start_t} to {end_t}.')
     if bulk:
+        logging.info("Bulk download enabled.")
         df = download_bulk_data(
             DOWNLOAD_TYPE_JOB,
             download_scheduler_event,
@@ -27,15 +30,50 @@ def job(vsn, start, end, bulk):
     else:
         df = download_scheduler_event(vsn, start_t, end_t)
 
-    logging.info(f'{len(df)} records found')
+    logging.info(f'{len(df)} records found.')
     if len(df) == 0:
-        logging.info(f'Query {vsn} {args.start} {args.end} returned 0 records')
+        logging.info(f'Query {vsn} {args.start} {args.end} returned 0 records.')
         return 0
 
+    logging.info(f'Parsing the records.')
     out_df = generate_job_records(df)
-    output = "jobs.csv"
     out_df.to_csv(output, index=False)
     logging.info(f'Created {output}. Done.')
+
+
+@cli.command()
+@click.option("-i", "--input", required=True, type=Path, default=Path("jobs.csv"), help="Path to the job list in CSV. Default is ./jobs.csv")
+@click.option("-r", "--resume", is_flag=True, default=False, help="Skip downloading if exists locally. It is useful when resuming downloading from where it left off.")
+@click.option("-o", "--output-dir", type=Path, default=Path("./"), help="Path to save the downloaded data. Default is the current directory.")
+def perf(input, resume, output_dir):
+    logging.info(f'Reading job data from {input}.')
+    df = pd.read_csv(input)
+
+    logging.info(f'Output directory is {output_dir}.')
+
+    logging.info("We consider only successfully completed runs, not the ones that are failed or unknown")
+    completed_runs = df[df["end_state"] == "completed"]
+    logging.info(f'{len(completed_runs)} completed runs found.')
+    logging.info(f'Completed runs by plugin name are {completed_runs.groupby("plugin_task").size()}')
+
+    # logging.info("Sorting the runs by plugin_name")
+    # completed_runs = completed_runs.sort_values(by="plugin_name")
+
+    for plugin_name, runs in completed_runs.groupby("plugin_name"):
+        logging.info(f'Downloading metrics for {plugin_name}')
+        plugin_df = pd.DataFrame()
+        plugin_output_path = output_dir.joinpath(f'{plugin_name}.csv')
+        if plugin_output_path.exists() and resume:
+            logging.info(f'{plugin_output_path} already exists. --resume is enabled. Skipping.')
+            continue
+
+        total_iteration = len(runs)
+        t = tqdm(range(total_iteration))
+        for i in t:
+            run = runs.iloc[i]
+            run_df = generate_metrics_from_instance(t, run)
+            plugin_df = pd.concat([plugin_df, run_df])
+        plugin_df.to_csv(plugin_output_path, index=False)
 
 
 if __name__ == "__main__":
@@ -45,5 +83,3 @@ if __name__ == "__main__":
         datefmt='%Y/%m/%d %H:%M:%S')
 
     cli()
-
-    # exit(main(args))
--- utils.py
+++ utils.py
@@ -169,6 +169,79 @@ def is_gpu_requested(plugin_instance_record: pd.Series):
     return False
 
 
+def generate_metrics_from_instance(t: tqdm, run: pd.Series):
+    instance = run.k3s_pod_instance
+    device = convert_nodename_to_devicename(run.k3s_pod_node_name)
+    gpu_required = is_gpu_requested(run)
+    plugin_name = run.plugin_name
+    vsn = run.vsn
+    started = pd.to_datetime(run.timestamp)
+    completed = pd.to_datetime(run.completed_at)
+    extended_started = pd.to_datetime(run.timestamp) - pd.to_timedelta(1, unit='m')
+    extended_completed = pd.to_datetime(run.completed_at) + pd.to_timedelta(1, unit='m')
+    extended_started = extended_started.isoformat()
+    extended_completed = extended_completed.isoformat()
+
+    t.write(f'{instance}: Fetching data from cloud ranging from {extended_started} to {extended_completed}')
+    perf_df = download_performance_data(vsn, extended_started, extended_completed)
+    if len(perf_df) < 1:
+        t.write(f'No record found for {instance}')
+        return pd.DataFrame()
+
+    container_perf_df = perf_df[perf_df["meta.container"]==plugin_name]
+    container_cpu_perf_df = container_perf_df[container_perf_df["name"]=="container_cpu_usage_seconds_total"]
+    t.write(f'{instance}: {len(container_cpu_perf_df)} CPU records found')
+    if len(container_cpu_perf_df) < 1:
+        cpu = pd.DataFrame([], columns=["timestamp", "cpu"])
+        cpu["timestamp"] = pd.to_datetime(cpu["timestamp"], utc=True)
+    else:
+        cpu = calculate_cpu_utilization_from_cpuseconds(container_cpu_perf_df.copy(), started)[["timestamp", "cpu"]]
+        cpu = cpu.sort_values(by="timestamp")
+
+    container_mem_rss_perf_df = container_perf_df[container_perf_df["name"]=="container_memory_rss"]
+    container_mem_workingset_perf_df = container_perf_df[container_perf_df["name"]=="container_memory_working_set_bytes"]
+    t.write(f'{instance}: {len(container_mem_workingset_perf_df)} Memory workingset records found')
+    if len(container_mem_workingset_perf_df) < 1:
+        mem = pd.DataFrame([], columns=["timestamp", "mem"])
+        mem["timestamp"] = pd.to_datetime(mem["timestamp"], utc=True)
+    else:
+        container_mem_workingset_perf_df["mem"] = container_mem_workingset_perf_df["value"].values + container_mem_rss_perf_df["value"].values
+        mem = container_mem_workingset_perf_df[["timestamp", "mem"]]
+    try:
+        mem = mem.sort_values(by="timestamp")
+        merged_instance = pd.merge_asof(cpu[["timestamp", "cpu"]], mem[["timestamp", "mem"]], on="timestamp")
+    except ValueError as ex:
+        t.write(f'{instance}: ERROR="{ex}" DATA={cpu}')
+        return pd.DataFrame()
+
+    if "meta.sensor" not in perf_df.columns:
+        t.write(f'{instance}: meta.sensor field not found. Unable to retrive power measurements')
+        t.write(f'{instance}: columns in the data are {perf_df.columns}')
+        pow = pd.DataFrame([], columns=["timestamp", "sys_power", "cpugpu_power"])
+        pow["timestamp"] = pd.to_datetime(pow["timestamp"], utc=True)
+        merged_instance = pd.merge_asof(merged_instance, pow, on="timestamp")
+    else:
+        tegra_total_power = perf_df[(perf_df["name"] == "tegra_wattage_current_milliwatts") & (perf_df["meta.sensor"] == "vdd_in")]
+        t.write(f'{instance}: {len(tegra_total_power)} tegra power metric records found')
+        tegra_total_power = tegra_total_power.rename({"value": "sys_power"}, axis="columns")
+        tegra_total_power = tegra_total_power.sort_values(by="timestamp")
+        merged_instance = pd.merge_asof(merged_instance, tegra_total_power[["timestamp", "sys_power"]], on="timestamp")
+
+        tegra_cpugpu_power = perf_df[(perf_df["name"] == "tegra_wattage_current_milliwatts") & (perf_df["meta.sensor"] == "vdd_cpu_gpu_cv")]
+        t.write(f'{instance}: {len(tegra_total_power)} tegra cpugpu power metric records found')
+        tegra_cpugpu_power = tegra_cpugpu_power.rename({"value": "cpugpu_power"}, axis="columns")
+        tegra_cpugpu_power = tegra_cpugpu_power.sort_values(by="timestamp")
+        merged_instance = pd.merge_asof(merged_instance, tegra_cpugpu_power[["timestamp", "cpugpu_power"]], on="timestamp")
+
+    # Merging all metrics
+    merged_instance["plugin_instance"] = instance
+    merged_instance["device"] = device
+    merged_instance["gpu_requested"] = gpu_required
+    merged_instance['timestamp'] = merged_instance['timestamp'].map(lambda x: x.isoformat())
+    t.write(f'{instance}: Generated {len(merged_instance)} records. Done.')
+    return merged_instance
+
+
 def convert_nodename_to_devicename(node_name: str) -> str:
     if "nx" in node_name:
         return "Jetson"
